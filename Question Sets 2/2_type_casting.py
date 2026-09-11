d6c8d037ad7b5913c2f8402c175200e1e4ab71d8
Hari369 <hiranmaymondal369@gmail.com>
2026-09-11 10:23:59 +0000

    adding question sets

---
+++ Question Sets 2/2_type_casting.py
@@ -0,0 +1,168 @@
+# # 1. Convert the string "100" into an integer and print its type.
+# a = "100"
+# b = int(a)
+# print(b)
+
+
+# # 2. Convert the integer 25 into a float and print the result and its type.
+# a = 25
+# b = float(a)
+# print(b)
+
+
+# # 3. Convert the float 99.99 into an integer. What will be the output?
+# a = 99.99
+# b = int(a)
+# print(b)
+
+
+# # 4. Convert the integer 500 into a string and concatenate it with "is my score"
+# a = "is my score"
+# b = "500"
+# print(a + b)
+
+
+
+# # 5. Convert the string "3.14" into a float and print the result.
+# a = "3.14"
+# b = float(a)
+# print(b)
+
+
+
+# # 6. What will be the output?
+# x = "10"
+# y = 20
+
+# print(x + str(y))
+
+
+
+# # # 7. What will happen here? Explain why
+# # x = "50"
+# # y = 10
+
+# # print(x + y)
+
+
+
+# # 8. Take two numbers from the user using input() and add them together.
+# a = int(input())
+# b = int(input())
+# print(a+b)
+
+
+
+# # 9. What will be the output?
+# x = 10.99
+# y = int(x)
+# z = str(y)
+
+# print(x)
+# print(y)
+# print(z)
+# print(type(z))
+
+
+
+# # 10. Write a program that asks the user for:
+# # Name
+# # Age
+# # Height in meters
+
+# # Then convert the age to int and height to float, and print them along with their data types
+
+# name = input("Enter Name: ")
+# age = int(input("Enter Age: "))
+# h_meter = float(input("Enter height"))
+
+
+
+# # 11. Take two numbers as input as strings, convert them to integers, and print their sum and difference.
+# a = int(input("Enter Number 1 : "))
+# b = int(input("Enter Number 2 : "))
+
+# c = a + b
+# int(c)
+# print(c)
+
+
+# # 12. Medium Take a number from the user as a string: "25.75"
+# a = "25.75"
+# b = float(a)
+# print(b)
+# print(type(b))
+
+# c = int(b)
+# print(c)
+# print(type(c))
+
+
+# # 13. Medium Take two numbers from the user and calculate their average. 10 20
+# a = int(input("Enter a number 1 : "))
+# b = int(input("Enter a number 2 : "))
+# c = (a + b) / 2
+# print(c)
+
+
+# # 1. Convert the string "25" and "15" into integers and find their sum.
+# a = int(input("Enter a number 1 : "))
+# b = int(input("Enter a number 2 : "))
+
+# c = a+b
+
+# print(c)
+
+
+
+# # 2. Take a number from the user as a string, convert it to a float, and calculate its square.
+# a = int(input("Enter a Number : "))
+# b = float(a)
+# c = b*b
+# print(c)
+
+
+
+# # 3. Convert the following values to integers and print the result:
+# a = "10"
+# b = "20"
+# c = "30"
+
+# a1 = int(a)
+# a2 = int(b)
+# a3 = int(c)
+
+# print(a1)
+# print(a2)
+# print(a3)
+
+
+
+# # 4. Take two numbers from the user using input(). One should be treated as an integer and the other as a float. Add them and print the result and its type.
+# a = int(input("Enter Number 1 : "))
+# b = float(input("Enter Number 2 : "))
+# c = a + b
+# print(c)
+
+
+
+# # 5. Given: 
+# a = "100" 
+# b = 25 
+# c = 10.5
+
+# x = float(a)
+# y = float(b)
+# z = float(c)
+
+# print(x+y+z)
+
+
+
+# # 6. Take a person's age as input. Convert it to an integer and determine their age after 5 years.
+# a = int(input("Enter a Age: "))
+# c = a + 5
+# print("Your Age is after 5 year : ", c)
+
+
+
